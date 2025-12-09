64bca1e8b83b6a50d3630363fabb82e784c2afd6
Walker <jahtemu@gmail.com>
2025-12-09 01:34:16 +0100

    more accurate control

--- main/ship.py
+++ main/ship.py
@@ -1,5 +1,6 @@
 import pygame
 import os
+from settings import settings
 
 
 class ship:
@@ -8,26 +9,33 @@ class ship:
     def __init__(self, game):
         self.screen = game.screen
         self.screen_rect = game.screen.get_rect()
+        self.settings = game.settings
 
         self.image = pygame.image.load("images\\rocket.png")
         self.rect = self.image.get_rect()
+        self.rect.midbottom = self.screen_rect.midbottom
+
+        self.x = float(self.rect.x)
+        self.y = float(self.rect.y)
+
         self.moving_left = False
         self.moving_right = False
         self.moving_up = False
         self.moving_down = False
 
-        self.rect.midbottom = self.screen_rect.midbottom
-
     def move(self):
         """update ship movement base on the movement flag"""
         if self.moving_right:
-            self.rect.x += 1
+            self.x += self.settings.ship_speed
         if self.moving_left:
-            self.rect.x -= 1
+            self.x -= self.settings.ship_speed
         if self.moving_up:
-            self.rect.y -= 1
+            self.y -= self.settings.ship_speed
         if self.moving_down:
-            self.rect.y += 1
+            self.y += self.settings.ship_speed
+
+        self.rect.x = self.x
+        self.rect.y = self.y
 
     def rocket(self):
         """drawing ship"""
