4844c51479f769943dd130a3134b18e5523aa61d
Walker <jahtemu@gmail.com>
2025-12-09 14:14:27 +0100

    control done

--- main/main.py
+++ main/main.py
@@ -33,25 +33,32 @@ class AlienInvasion:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+            elif event.type == pygame.K_q:
+                sys.exit()
             elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RIGHT:
-                    self.ship.moving_right = True
-                elif event.key == pygame.K_LEFT:
-                    self.ship.moving_left = True
-                elif event.key == pygame.K_UP:
-                    self.ship.moving_up = True
-                elif event.key == pygame.K_DOWN:
-                    self.ship.moving_down = True
-
+                self._checkdown_event(event)
             elif event.type == pygame.KEYUP:
-                if event.key == pygame.K_RIGHT:
-                    self.ship.moving_right = False
-                elif event.key == pygame.K_LEFT:
-                    self.ship.moving_left = False
-                elif event.key == pygame.K_UP:
-                    self.ship.moving_up = False
-                elif event.key == pygame.K_DOWN:
-                    self.ship.moving_down = False
+                self._checkup_event(event)
+
+    def _checkdown_event(self, event):
+        if event.key in (pygame.K_RIGHT, pygame.K_d):
+            self.ship.moving_right = True
+        elif event.key in (pygame.K_LEFT, pygame.K_a):
+            self.ship.moving_left = True
+        elif event.key in (pygame.K_UP, pygame.K_w):
+            self.ship.moving_up = True
+        elif event.key in (pygame.K_DOWN, pygame.K_s):
+            self.ship.moving_down = True
+
+    def _checkup_event(self, event):
+        if event.key in (pygame.K_RIGHT, pygame.K_d):
+            self.ship.moving_right = False
+        elif event.key in (pygame.K_LEFT, pygame.K_a):
+            self.ship.moving_left = False
+        elif event.key in (pygame.K_UP, pygame.K_w):
+            self.ship.moving_up = False
+        elif event.key in (pygame.K_DOWN, pygame.K_s):
+            self.ship.moving_down = False
 
     def _update_screen(self):
         self.screen.fill(self.settings.bg_color)
--- main/settings.py
+++ main/settings.py
@@ -4,5 +4,5 @@ class settings:
     def __init__(self):
         self.screen_width = 1200
         self.screen_height = 800
-        self.bg_color = (100, 100, 230)
-        self.ship_speed = 1.5
+        self.bg_color = (0, 30, 0)
+        self.ship_speed = 8
